e2e619ae8ccaa21289d3abe432bb046a2e5ccac7
Andy Sazima <andysazima@gmail.com>
2024-08-24 19:56:31 -0700

    initial commit of nvim init files

---
+++ lua/config/keymap.lua
@@ -0,0 +1,22 @@
+local g = vim.g
+local map = vim.keymap.set
+
+g.mapleader = " "
+g.maplocalleader = "\\"
+
+map("i", "jk", "<Esc>", {desc="escape from insert mode with 'jk' pressed in succession"})
+
+map("n", "<C-h>", "<C-w>h", {desc="switch window left without the extra <C-w> press"})
+map("n", "<C-j>", "<C-w>j", {desc="switch window down without the extra <C-w> press"})
+map("n", "<C-k>", "<C-w>k", {desc="switch window up without the extra <C-w> press"})
+map("n", "<C-l>", "<C-w>l", {desc="switch window right without the extra <C-w> press"})
+
+map("n", "<leader>fe", "<cmd>Neotree toggle<CR>", {noremap = "true", desc="Toggle Neotree"})
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", {noremap = "true", desc="Find files with Telescope"})
+
+map("n", "<M-up>", "<cmd>m .-2<CR>==", {noremap = "true"})
+map("n", "<M-down>", "<cmd>m .+1<CR>==", {noremap = "true"})
+
+map("n", "<C-u>", "<C-u>zz", {noremap = "true"})
+map("n", "<C-d>", "<C-d>zz", {noremap = "true"})
+
---
+++ lua/config/set.lua
@@ -0,0 +1,24 @@
+local g = vim.g
+local set = vim.opt
+
+set.number = true
+set.relativenumber = true
+
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+
+set.smartindent = true
+
+set.wrap = false
+
+set.swapfile = false
+set.backup = false
+
+set.incsearch = true
+set.termguicolors = true
+
+set.scrolloff = 8
+set.signcolumn = "yes"
+
